deba36a797b2a30961af304f3d28ac5ba49c576d
AngelGamboa <angel.gamboa@unosquare.com>
2024-05-15 11:36:05 -0600

    finished hamburguer menu test

--- src/test/java/test/testRunner.java
+++ src/test/java/test/testRunner.java
@@ -1,13 +1,12 @@
 package test;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features="src/test/java/features/",glue={"stepDefinitions"},stepNotifications = true)
+@CucumberOptions(features="src/test/resources/features",glue="stepDefinitions",plugin = {"pretty", "html:target/cucumber-reports"})
 
 public class testRunner {
 
---
+++ src/test/resources/features/verifyCategory.feature
@@ -0,0 +1,11 @@
+
+@tag
+Feature: Validate that category section in the left hamburguer menu shows correct elements
+
+  @tag1
+  Scenario: Validate that correct categories displays at the left hamburger menu
+    Given Amazon web page 
+    When validate categories menu
+
+
+ 
